010a3c820676f285dac79167b5bb44af6b653f4b
Pierre Olivier Martel <pomartel@coderubik.com>
2026-01-15 11:32:58 -0500

    remove year from url

--- src/middleware.js
+++ src/middleware.js
@@ -1,3 +1,12 @@
 export const onRequest = async (context, next) => {
+  const { pathname, search } = context.url;
+  const match = pathname.match(/^(\/posts)?\/(2025|2026)\/(.+)$/);
+
+  if (match) {
+    const prefix = match[1] ?? "";
+    const target = `${prefix}/${match[3]}${search}`;
+    return context.redirect(target, 301);
+  }
+
   return next();
 };

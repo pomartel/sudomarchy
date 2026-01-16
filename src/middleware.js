317fa0901fc69a78790702cf36763dd75446a8c6
Pierre Olivier Martel <pomartel@coderubik.com>
2026-01-16 13:45:20 -0500

    Remove FormattedDate

--- src/middleware.js
+++ src/middleware.js
@@ -1,12 +1,1 @@
-export const onRequest = async (context, next) => {
-  const { pathname, search } = context.url;
-  const match = pathname.match(/^(\/posts)?\/(2025|2026)\/(.+)$/);
-
-  if (match) {
-    const prefix = match[1] ?? "";
-    const target = `${prefix}/${match[3]}${search}`;
-    return context.redirect(target, 301);
-  }
-
-  return next();
-};
+export const onRequest = async (context, next) => {};
